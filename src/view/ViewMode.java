a380437cee96b04b4e7472b1ba48edb6e7fb1967
ThomasCloarec <thomas.cloarec.d@gmail.com>
2020-04-24 23:37:11 +0200

    Add red dragon gif

--- src/view/ViewMode.java
+++ src/view/ViewMode.java
@@ -3,5 +3,15 @@ package view;
 public enum ViewMode {
     TEXTUAL,
     GRAPHICAL_2D,
-    GRAPHICAL_3D
+    GRAPHICAL_3D;
+
+    public static boolean contains(String test) {
+        for (ViewMode c : ViewMode.values()) {
+            if (c.name().equals(test)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
